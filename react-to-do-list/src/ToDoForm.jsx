f31df52e3adcf611829aba2dec6350674da2c45c
jenvuong <jennifervuong4@gmail.com>
2024-08-05 21:58:08 +1200

    Update ToDoForm.jsx

--- react-to-do-list/src/ToDoForm.jsx
+++ react-to-do-list/src/ToDoForm.jsx
@@ -1,10 +1,28 @@
+import { useState } from "react"
+
 export function ToDoForm () {
+    const [inputState, setInputState] = useState("")
+    const [toDoArray, setToDoArray] = useState([])
+
+    function handleSubmit(e) {
+        e.preventDefault()
+
+       
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <h1>My To Do List.</h1>
             <p>What needs to be done today?</p>
             <div className="input-set">
-                <input className='input-box' type="text" placeholder='Start typing...' />
+                <input 
+                    value={inputState} 
+                    onChange={e => setInputState(e.target.value)} 
+                    className='input-box' 
+                    type="text" 
+                    placeholder='Start typing...'
+                    id='taskInput' 
+                />
                 <button className='add-btn'>Add</button>
             </div>
         </form>
